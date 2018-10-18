aa3ad2d5946ec56433467e9117d765fd9489a2dd
Jrodr4544 <maskdbandit@gmail.com>
2018-10-18 00:02:38 +0000

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,24 @@
 // Component Code Goes Here
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+import Address from './components/Address';
+ 
+
+    class Address extends React.Component {
+      render() {
+        return (
+          <div className="address">{this.props.street}, {this.props.city}</div>
+        );
+      }
+    }
+
+ReactDOM.render(
+  <Address 
+    street="Santa Monica Blvd."
+    city="Santa Monica"
+  />,
+  document.getElementById('root')
+);
+
